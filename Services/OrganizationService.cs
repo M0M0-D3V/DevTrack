904e876444b1463c9af596139da23992fc547ce8
Monica Hong <momo.star@gmail.com>
2020-08-16 00:28:39 -0700

    Org services

--- Services/OrganizationService.cs
+++ Services/OrganizationService.cs
@@ -1,7 +1,22 @@
+using DevTrack.Data;
+using DevTrack.Entities;
+
 namespace DevTrack.Services
 {
     public class OrganizationService
     {
-        
+        private DataContext _context;
+
+        public OrganizationService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Organization Create(Organization organization)
+        {
+            _context.Organizations.Add(organization);
+            _context.SaveChanges();
+            return organization;
+        }
     }
 }
